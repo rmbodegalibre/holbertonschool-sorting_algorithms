c10d4ea6ed38763a5daee4d550bbad39cad7fc0b
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-12 14:01:38 -0500

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+ * swapper - function to swap nodes of a doubly linked list.
+ * @list: doubly linked list
+ * @current_node: current node
+ * @previous_node: previous node
+ */
+
+void swapper(listint_t **list, listint_t *current_node, listint_t *previous_node)
+{
+    if (previous_node->prev)
+        previous_node->prev->next = current_node;
+    else
+        *list = current_node;
+
+    if (current_node->next)
+        current_node->next->prev = previous_node;
+
+    current_node->next = previous_node;
+    current_node->prev = previous_node->prev;
+}
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of integers
+ * in ascending order using insertion sort algorithm
+ * @list: doubly linked list to sort
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current_node = NULL;
+    listint_t *next_to_current = NULL, *previous_to_current = NULL;
+
+    if (!list || !*list)
+        return;
+
+    current_node = *list;
+
+    while (current_node)
+    {
+        previous_to_current = current_node->prev;
+        next_to_current = current_node->next;
+
+        while (previous_to_current && current_node->n < previous_to_current->n)
+        {
+            previous_to_current->next = current_node->next;
+            swapper(list, current_node, previous_to_current);
+            previous_to_current->prev = current_node;
+            previous_to_current = current_node->prev;
+
+            print_list(*list);
+        }
+        current_node = next_to_current;
+    }
+}
